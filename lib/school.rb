b72c9b3cb5820064156ede06410e7b039670ae13
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-07 01:43:21 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,14 +2,15 @@ class School
 
 attr_accessor :name, :roster, :student
 
-STUDENTS = []
+
 
   def initialize(name)
     @name =name
     @roster ={}
   end
 
-  def add_student(student)
-    STUDENTS << student
+  def add_student(student,grade)
+  @roaster =[:student,[grade]]
+
   end
 end
