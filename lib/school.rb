ff1db5475a97404681a03384d793e27bc34ae396
alejandra cantu <alejandra.cantu13@gmail.com>
2019-07-07 01:40:27 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -2,10 +2,14 @@ class School
 
 attr_accessor :name, :roster, :student
 
+STUDENTS = []
+
   def initialize(name)
     @name =name
     @roster ={}
   end
 
+  def student_name(student)
+    STUDENTS << student
 
 end
